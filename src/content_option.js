bc71d9e64dded63202a521d91ead1ebf6725be49
Waled Kamal <waledkama2510@gmail.com>
2022-08-24 17:08:14 +0200

    Last

---
+++ src/content_option.js
@@ -0,0 +1,179 @@
+const logotext = "Waleed Kamal";
+const meta = {
+  title: "Waleed Kamal",
+  description:
+    "I’m Javascript Developer , React , React Native ,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Waleed Kamal",
+  animated: {
+    first: "JavaScript Developer",
+    second: "React Developer",
+    third: "React Native Developer",
+  },
+  description: `Javascript Developer / App Developer offering 2+ years of experience`,
+  your_img_url: "",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme: ` Creative Front-End Developer / App Developer offering 2+ years of experience
+    providing high-impact web solutions for diverse industry
+    organizations &amp; literary institutions . Skilled in
+    digning, developing and testing multiple web-based
+    applications incorporating a range of technologies. Aspiring
+    to combine broad background with strong technical skills to
+    excel as a Front-End Developer.`,
+};
+const worktimeline = [
+    {
+        jobtitle: "React Native Developer",
+        where: "Hamyas Development - Part-time",
+        date: "Sep 2020 - Present · 4 yrs",
+      },
+  {
+    jobtitle: "Software Developer",
+    where: "Mostaql.com | مستقل · Freelance",
+    date: "Sep 2018 - Present · 4 yrs",
+  },
+  {
+    jobtitle: "Shopify Developer",
+    where: "Diamond ART DK · Part-time",
+    date: "Jul 2019 - Jul 2022 · 3 yrs 1 mo",
+  },
+  {
+    jobtitle: "Junior Java Developer",
+    where: "Foundation for Digital Creativity · Internship",
+    date: "Jan 2018 - Jul 2018 · 7 mos",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML & CSS",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 85,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "React Native",
+    value: 70,
+  },
+  {
+    name: "Node.js",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "FrontEnd Development",
+    description: `
+Develop functional and appealing web- and mobile-based applications based on usability,
+Provide website maintenance and enhancements,
+Use a combination of markup languages to write web pages,
+Write functional requirement documents and specifications,
+Create quality mockups and prototypes on tight timelines,
+Assist back-end developers with coding and troubleshooting,
+Create cascading style sheets (CSS) that are consistent across all browsers and platforms,
+Maintain graphic standards and branding throughout the product’s interface,
+Stay up-to-date on emerging technologies,
+Promote usability best practices`,
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      `Design and implement user interface components for JavaScript-based web and mobile applications using React ecosystem,
+      Work as a part of a small team that includes other react native developers, designers, QA experts, and managers,
+      Build app and UI components from prototypes and wireframes,
+      Work with native modules as and when required,
+       Use Native APIs for tight integrations with both platforms – Android and iOS,
+      Write automated tests to ensure error-free code and performance,
+      Improve front-end performance by eliminating performance bottlenecks,
+      Create front-end modules with maximum code reusability and efficiency,
+      Implement clean, smooth animations to provide an excellent user interface,
+      Work with third-party dependencies and APIs,
+      Work with Redux architecture to improve performance of the websites/mobile apps,
+      Coordinate with the cross-functional teams to build apps in stipulated time/budget.`,
+  },
+  {
+    title: "Node.js",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C5622AQEQWwNGM7C3SQ/feedshare-shrink_2048_1536/0/1660801131061?e=1664409600&v=beta&t=5DYiRiCUGzjbDhqdxtx7woEXFuq77BuwJksfDONJ3ws",
+    desctiption:
+      "A sports application that aims to follow the matches of the five leagues (the Spanish League - the Italian League - the English League - the French League - the German League)",
+    link: "https://play.google.com/store/apps/details?id=com.top5soccer.com",
+  },
+  {
+    img: "https://raw.githubusercontent.com/WaledKamal/react-chat-ui-engine/master/perview.png",
+    desctiption:
+      "react-chat-ui-template",
+    link: "https://waledkamal.github.io/react-chat-ui-engine/",
+  },
+  {
+    img: "https://alfalah-app.com/assets/imgs/app_Img_header.svg",
+    desctiption:
+      "The Al-Falah application is a farmer for every Muslim that provides many services for reading the Qur’an, supplications...",
+    link: "https://alfalah-app.com/",
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C5622AQETFncFomKuBQ/feedshare-shrink_2048_1536/0/1646489044362?e=1664409600&v=beta&t=WV14onYQJFJBprinO3KR3a5LXy9EvObHWPbcW7TZuSc",
+    desctiption:
+      "Ser Alard is an application that serves the agricultural community in all its fields, agricultural guidance, trade, shopping and jobs",
+    link: "https://play.google.com/store/apps/details?id=com.ZER.serard&hl=en_US&gl=UShttps://alfalah-app.com/",
+  },
+  ,
+  {
+    img: "https://github.com/WaledKamal/react-pos/blob/master/perview.png?raw=true",
+    desctiption:
+      "react.js POS",
+    link: " https://github.com/WaledKamal/react-pos",
+  }
+
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "waledkama2510@gmail.com",
+  YOUR_FONE: "+20 (010) 1750-4320",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_pwevhit",
+  YOUR_TEMPLATE_ID: "template_ge4ho45",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/WaledKamal",
+  facebook: "https://facebook.com/waledkamalfikrey",
+  linkedin: "https://www.linkedin.com/in/waledkamal",
+  twitter: "https://twitter.com/waledkamalfikrey",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
